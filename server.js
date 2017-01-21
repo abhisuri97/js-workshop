f40299bcf2a211a29c7e782e36c261a644108fec
Abhi <suriabhinav1997@gmail.com>
2017-01-20 23:10:36 -0500

    bound to port

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ io.on('connection', function(socket) {
   });
 });
 
-http.listen(3000, function() {
-  console.log('listening on 3000');
+http.listen(process.env.PORT || 3000, function() {
+  console.log('listening on ' + process.env.PORT || 3000);
 });
 
