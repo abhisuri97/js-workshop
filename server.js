5b22b34491652b94b54bbc320b86cdc4325b2e15
Abhi <suriabhinav1997@gmail.com>
2017-11-18 00:46:41 -0500

    simplified

--- server.js
+++ server.js
@@ -14,37 +14,17 @@ app.get('/', function (req, res) {
 io.on('connection', function(socket) {
   // adds user if they are unique username otherwise asks for new one
   // via pickNew event
-  socket.on('askUser', function(potentialUsername) {
-    var justUsers = users.map(function (el) {
-      return el.userName
-    });
-
-    // if we find a username already === potentialUsername emit event
-    // to prompt user again
-    if (justUsers.includes(potentialUsername)) {
-      socket.emit('pickNew');
-    } else {
-      // push a "user object" with unique id and potentialUsername
-      users.push({ id: socket.id, userName: potentialUsername });
-    }
-  });
-
   socket.on('chat message', function(msg) {
-    var username = "";
-    for (var i = 0; i < users.length; i++) {
-      if (users[i].id === socket.id) {
-        var username = users[i].userName
-      }
-    }
-    var msgObj = { message: msg, user: username }
+    var msgObj = { message: msg, user: socket.id }
     io.emit('chat message', msgObj); 
   });
+
   socket.on('disconnect', function() {
     console.log('a user disconnected');
   });
 });
 
 http.listen(process.env.PORT || 3000, function() {
-  console.log('listening on ' + process.env.PORT || 3000);
+  console.log('listening on ' + (process.env.PORT || 3000));
 });
 
